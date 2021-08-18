f3def6293bcd9ca31ef7ad7e94418027d5c2c1cc
Thiago Muta <thimuta@hotmail.com>
2021-08-18 17:44:13 -0300

    Rails Wikinimous terminar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Faker::Games::SuperMario.game
+puts 'Creating 10 fake restaurants...'
+10.times do
+  article = Article.new(
+    title: Faker::Company.name,
+    content: Faker::Games::SuperMario.game,
+  )
+  article.save!
+end
+puts 'Finished!'
